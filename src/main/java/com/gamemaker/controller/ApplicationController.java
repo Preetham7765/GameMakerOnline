44b9d77fde93a1fd67e8385a9b9db3f05c1ae724
Ankita Rajendra Alshi <ankita@140-182-205-140.dhcp-bl.indiana.edu>
2018-10-12 15:53:13 -0400

    jnlp integration

--- src/main/java/com/gamemaker/controller/ApplicationController.java
+++ src/main/java/com/gamemaker/controller/ApplicationController.java
@@ -1,33 +1,27 @@
 package com.gamemaker.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.gamemaker.model.ScoreBoardEntry;
 import com.gamemaker.service.ScoreBoradService;
 
 @RestController
 public class ApplicationController {
 
 	@Autowired
-	private ScoreBoradService scoreBoardService_;
+	private ScoreBoradService scoreBoardService;
 	
 	@GetMapping("/scoreboard")
 	public String getScoreBoard() {
-		
-		
+
 		return "Hello! Welcome to our game maker";
 		
 	}
 	
-	
 	@PostMapping("/scoreboard")
 	public void addNewScoreBoardEntry(){
-		
 		
 	}
 	
--- src/main/java/com/gamemaker/entity/PlayerDetails.java
+++ src/main/java/com/gamemaker/entity/PlayerDetails.java
@@ -2,24 +2,35 @@ package com.gamemaker.entity;
 
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class PlayerDetails {
 
-	@EmbeddedId
-	private PlayerDetailId id;
+	@Id
+	private int id;
+	
+	private int gameId;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getGameId() {
+		return gameId;
+	}
+
+	public void setGameId(int gameId) {
+		this.gameId = gameId;
+	}
 
 	private String name;
 
 	private String score;
-
-	public PlayerDetailId getId() {
-		return id;
-	}
-
-	public void setId(PlayerDetailId id) {
-		this.id = id;
-	}
 
 	public String getName() {
 		return name;
